40f4aadf76bf6f93bc82c38b0035d3fd362449d6
Shaikh Sahaab <i.am.samir0725@gmail.com>
2025-07-19 20:35:47 +0530

    service-wroker.js

--- service-wroker.js
+++ service-wroker.js
@@ -9,14 +9,28 @@ const urlsToCache = [
   '/icon-512.png'
 ];
 
+// Install event: cache app shell
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
-    })
+    }).then(() => self.skipWaiting()) // Activate new service worker immediately
   );
 });
 
+// Activate event: clean up old caches if needed
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch event: respond with cached resources or fetch from network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
